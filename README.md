c33407599d7fcc40c004959f926d3043286244ac
Laura Burticioaia <lburticioaia@luxoft.com>
2017-03-27 14:26:56 +0300

    Project

---
+++ README.md
@@ -0,0 +1,2 @@
+This package installs the script from the scapy task.
+After running 'pip install PythonProject' you should be able to immediately execute(from any directory) the command 'scapy_task'.
--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
-try:
-	from setuptools import setup
-except ImportError:
-	from distutils.core import setup
-
-config = {
-	'description': 'Python skelet',
-	'author': 'Laura Burticioaia',
-	'author_email': 'lburticioaia@luxoft.com',
-	'version': '0.1',
-	'name': 'Python Project'
-	'url': 'https://github.com/Lamabu/skeleton',
-	'download_url':'https://github.com/Lamabu/skeleton/archive/0.1.tar.gz'
-}
+from distutils.core import setup
 
 setup(
+	description='Python skelet',
+	author='Laura Burticioaia',
+	author_email='lburticioaia@luxoft.com',
+	version='0.1',
+	name='PythonProject',
+	url='https://github.com/Lamabu/skeleton',
+	download_url='https://github.com/Lamabu/skeleton/archive/0.1.tar.gz',
 	scripts=['bin/scapy_task.py']
 )	
